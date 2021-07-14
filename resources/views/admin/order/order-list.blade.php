f9196bde841a9a71d288adaf0f64527acfe1ea1b
Hau Mickey <haunguyen0603@gmail.com>
2021-07-14 17:55:27 +0700

    fix

--- resources/views/admin/order/order-list.blade.php
+++ resources/views/admin/order/order-list.blade.php
@@ -1,70 +1,44 @@
 @extends('admin_layout')
 @section('admin_content')
-    <style>
-        body { padding-top:20px; }
-        .panel-body .btn:not(.btn-block) { width:120px;margin-bottom:10px; }
-        .container {
-            width: 100%;
-        }
-    </style>
-    <div class="inner-header">
-        <div class="container">
-            <div class="pull-left">
-                <h3 class="inner-title">Danh Sách Hóa Đơn</h3>
-            </div>
-            {{-- <div class="pull-right">
-                <div class="beta-breadcrumb">
-                <h4><a href="{{route('page.admin')}}">Back to Admin</a></h4>
-                </div>
-            </div> --}}
-            <div class="clearfix"></div>
-        </div>
-    </div>
-    <div class="container">
-        <div class="panel panel-default">
-            <div class="panel-body">
-                <div class="table-responsive">
-                    <table class="table table-condensed" id="table">
-                        <thead>
-                        <tr>
-                            <td width="5%"><strong>ID</strong></td>
-                            <td width="10%"><strong>Order Date</strong></td>
-                            <td width="15%"><strong>Customer Name</strong></td>
-                            <td width="10%"><strong>Payment</strong></td>
-                            <td width="25%"><strong>Note</strong></td>
-                            <td width="5%"><strong>Amount</strong></td>
-                            <td width="10%"><strong>Total</strong></td>
-                            {{-- <td width="10%"><strong>Status</strong></td> --}}
-                            <td width="10%"><strong>Action</strong></td>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {{-- @foreach($order as $key => $bill)
-                        <tr>
-                            <td>{{$bill->order_id}}</td>
-                            <td>{{$bill->date_order}}</td>
-                            <td>{{$bill->name}}</td>
-                            <td>{{$bill->payment}}</td>
-                            <td>{{$bill->note}}</td>
-                            <td>{{$bill->products}}</td>
-                            <td>{{$bill->total}}</td>
-                            <form action="{{route('delete_order',$bill->order_id)}}" method="post" class="beta-form-checkout">
-                                <input type="hidden" name="_token" value="{{csrf_token()}}">
-                                {{ method_field('DELETE') }}
-                                <td><button type="submit" class="btn btn-danger btn-block" onclick="return confirm('Bạn muốn xóa không?')">Xóa</button></td>
-                            </form>
-                        </tr>
-                        @endforeach --}}
-                        </tbody>
-                    </table>
-                </div>
+    <div class="panel panel-default">
+        <div class="panel-heading"><b>Danh sách đơn hàng</b></div>
+        <div class="panel-body">
+            <div class="table-responsive">
+                <table class="table table-condensed" id="table">
+                    <thead>
+                    <tr>
+                        <td width="5%"><strong>ID</strong></td>
+                        <td width="10%"><strong>Order Date</strong></td>
+                        <td width="15%"><strong>Customer Name</strong></td>
+                        <td width="10%"><strong>Payment</strong></td>
+                        <td width="25%"><strong>Note</strong></td>
+                        <td width="5%"><strong>Amount</strong></td>
+                        <td width="10%"><strong>Total</strong></td>
+                        {{-- <td width="10%"><strong>Status</strong></td> --}}
+                        <td width="15%"><strong>Action</strong></td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {{-- @foreach($order as $key => $bill)
+                    <tr>
+                        <td>{{$bill->order_id}}</td>
+                        <td>{{$bill->date_order}}</td>
+                        <td>{{$bill->name}}</td>
+                        <td>{{$bill->payment}}</td>
+                        <td>{{$bill->note}}</td>
+                        <td>{{$bill->products}}</td>
+                        <td>{{$bill->total}}</td>
+                        <form action="{{route('delete_order',$bill->order_id)}}" method="post" class="beta-form-checkout">
+                            <input type="hidden" name="_token" value="{{csrf_token()}}">
+                            {{ method_field('DELETE') }}
+                            <td><button type="submit" class="btn btn-danger btn-block" onclick="return confirm('Bạn muốn xóa không?')">Xóa</button></td>
+                        </form>
+                    </tr>
+                    @endforeach --}}
+                    </tbody>
+                </table>
             </div>
         </div>
-        {{-- <div class="row">
-            <div class="col-md-12">
-                
-            </div>
-        </div> --}}
     </div>
     <script type="text/javascript">
         $(document).ready(function () {
--- resources/views/admin/product-type/product-type-list.blade.php
+++ resources/views/admin/product-type/product-type-list.blade.php
@@ -1,53 +1,27 @@
 @extends('admin_layout')
 @section('admin_content')
-    <style>
-        body { padding-top:20px; }
-        .panel-body .btn:not(.btn-block) { width:120px;margin-bottom:10px; }
-        .container {
-            width: 100%;
-        }
-    </style>
-    <div class="inner-header">
-        <div class="container">
-            <div class="pull-left">
-                <h3 class="inner-title">Loại sản phẩm</h3>
-            </div>
-            {{-- <div class="pull-right">
-                <div class="beta-breadcrumb">
-                <h4><a href="{{route('page.admin')}}">Back to Admin</a></h4>
-                </div>
-            </div> --}}
-            <div class="clearfix"></div>
-        </div>
-    </div>
-    <div class="container">
-        <div class="panel panel-default">
-            <div class="panel-body">
-                <div class="table-responsive">
-                    <table class="table table-condensed" id="table">
-                        <thead>
-                        <tr>
-                            <td width="5%"><strong>ID</strong></td>
-                            <td width="10%"><strong>Name</strong></td>
-                            <td width="35%"><strong>Description</strong></td>
-                            <td width="15%"><strong>Created at</strong></td>
-                            <td width="15%"><strong>Updated at</strong></td>
-                            <td width="10%"><strong>Active</strong></td>
-                            <td width="10%"><strong>Action</strong></td>
-                        </tr>
-                        </thead>
-                        <tbody>
+    <div class="panel panel-default">
+        <header class="panel-heading"><b>Danh sách Loại sản phẩm</b></header>
+        <div class="panel-body">
+            <div class="table-responsive">
+                <table class="table table-condensed" id="table">
+                    <thead>
+                    <tr>
+                        <td width="5%"><strong>ID</strong></td>
+                        <td width="10%"><strong>Name</strong></td>
+                        <td width="35%"><strong>Description</strong></td>
+                        <td width="15%"><strong>Created at</strong></td>
+                        <td width="15%"><strong>Updated at</strong></td>
+                        <td width="10%"><strong>Active</strong></td>
+                        <td width="10%"><strong>Actions</strong></td>
+                    </tr>
+                    </thead>
+                    <tbody>
 
-                        </tbody>
-                    </table>
-                </div>
+                    </tbody>
+                </table>
             </div>
         </div>
-        {{-- <div class="row">
-            <div class="col-md-12">
-                
-            </div>
-        </div> --}}
     </div>
     <script type="text/javascript">
         $(document).ready(function () {
